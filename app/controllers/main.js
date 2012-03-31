446eb8b3da03de8b2543989fb200b5dbeb38e004
jerhay <jeremy@jhay.net>
2012-03-31 13:17:14 -0700

    make fb connection stuff use ajax utils

--- app/controllers/main.js
+++ app/controllers/main.js
@@ -19,6 +19,8 @@
 var Main = function () {
   var QueryString = require("querystring"),
       https = require("https");
+      ajax = geddy.ajax;
+  var fbgraph = "https://graph.facebook.com";
   var fbStateLookup = "fbstate";
   this.index = function (req, resp, params) {
     this.respond(params, {
@@ -45,6 +47,44 @@ var Main = function () {
     this.redirect("https://www.facebook.com/dialog/oauth?client_id=405186406159531&redirect_uri=http%3A%2F%2Fzoopbloop.com%2Freturn&state="+fbstate);
   }
 
+  function getMe(token,expires,cb)
+  {
+    // todo check for errors
+    ajax.get(fbgraph +"/me?access_token=" + token ,function(res,data){
+      if(res.statusCode==200)
+      {
+        var me = JSON.parse(data);
+        geddy.log.info(me.id);
+        geddy.log.info(me.name);
+        geddy.model.User.load(me.id,function(user){
+          if(!user)
+          {
+            //dont have this user yet, need to create a new one
+            // todo: replace expires with now + expires
+            user = geddy.model.User.create({ 
+              id: me.id,
+              name: me.name,
+              token: token,
+              expires: new Date()});
+            geddy.model.User.create(me.id,function(user){});
+          }
+          else
+          {
+            user.token = token;
+            user.expires = new Date();
+          }
+
+          user.save();
+          cb(user);
+        });
+      }
+      else
+      {
+        cb(null);
+      }
+    });
+  }
+
   this.returnFromFacebook = function(req,resp,params){
     //todo: make this query parsing into a util function
     geddy.log.info("in return from facebook. url:" + req.url)
@@ -56,6 +96,11 @@ var Main = function () {
     var fbstate = this.session.get(fbStateLookup);
     var done = function(data){
       self.respond(data?data:params,{format:'html',template:'app/views/main/return'});
+    }
+    var errorhandler = function()
+    {
+      geddy.log.error("oh snap, got an error in returnFromFacebook");
+      done(null);
     }
     geddy.log.info("query: " + query);
 
@@ -74,89 +119,32 @@ var Main = function () {
         + "&redirect_uri=http%3A%2F%2Fzoopbloop.com%2Freturn"
         + "&client_secret=693ab1df0adcfe0975b9896e751279d7"
         + "&code="+code;
-
-      var options = {
-        host: "graph.facebook.com",
-        port: 443,
-        path: fbtokenpath
-      };
-      
-      geddy.log.info("doing a https call");
-
       // need to handle errors properly
-      https.get(
-        options,
-        function(res){
+      ajax.get(
+        fbgraph+fbtokenpath,
+        function(res,data){
           var success = res.statusCode==200;
-          var data = [];
-          geddy.log.info("got a complete"); 
-          res.on("data",function(chunk){data.push(chunk); geddy.log.info("got a chunk");})
-          res.on("close",function(){ geddy.log.info("got a close event");})
-          res.on("end",function(){
-            geddy.log.info("got a end data "+ data);
-
-            if(success && data)
-            {
-              geddy.log.info("Yay we got a token");
-              
-              var qs = QueryString.parse(data.join());
-              var token = qs.access_token;
-              geddy.log.info("it expires in " + qs.expires);
-              options.path = "/me?access_token=" + token;
-              // todo check for errors
-              https.get(options,function(res){
-                var data = [];
-                if(res.statusCode==200)
-                {
-                  res.on("data",function(chunk){data.push(chunk)});
-                  res.on("close",function(){ geddy.log.info("got a close event");});
-                  res.on("end",function(){
-                    var me = JSON.parse(data.join());
-                    geddy.log.info(me.id);
-                    geddy.log.info(me.name);
-                    geddy.model.User.load(me.id,function(user){
-                      if(!user)
-                      {
-                        //dont have this user yet, need to create a new one
-                        // todo: replace expires with now + expires
-                        user = geddy.model.User.create({ 
-                          id: me.id,
-                          name: me.name,
-                          token: token,
-                          expires: new Date()});
-                        geddy.model.User.create(me.id,function(user){});
-                      }
-                      else
-                      {
-                        user.token = token;
-                        user.expires = expires;
-                      }
-                      user.save();
-                      done();
-                    });
-                    
-                  });
-                }
-              });
-              done()
-            }
-          });
-          res.on("error",function(e){
-            geddy.log.error("in error")
-            geddy.log.error(e);
-          });
-        }).on("error",function(e){
-          geddy.log.error("in error")
-          geddy.log.error(e);
-          done();
-          });
+          
+          if(success && data)
+          {
+            geddy.log.info("Yay we got a token");
+            var qs = QueryString.parse(data);
+            var token = qs.access_token;
+            getMe(token,qs.expires,done);
+          }
+          else
+          {
+            errorhandler()
+          }
+        },
+        errorhandler
+      );
     }
     else
     {
       self.respond({},{format:'html',template:'app/views/main/return'});
     }
   };
-
 };
 
 exports.Main = Main;
